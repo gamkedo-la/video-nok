88aceadc19bce51778fbb19ceee95a17861ed590
Ashleigh Morris <ash.morris255@gmail.com>
2020-05-20 11:48:54 -0400

    readded "world's worst AI" code snippet and commented it out

--- scripts/input.js
+++ scripts/input.js
@@ -40,6 +40,18 @@ function handleMouseUp(evt){
 		//ballOne.velX = Math.max(Math.min((shootStart.x-shootEnd.x)/5, 10), -10);
 		//ballOne.velY = Math.max(Math.min((shootStart.y-shootEnd.y)/5, 10), -10);
 
+		//pretend this is an AI for right now. 
+
+		/*
+		var randomFloat = Math.random() * 30;
+		console.log(randomFloat);
+
+		if(activePlayer == 2){
+			ballOne.velX = Math.max(Math.min(randomFloat, 30), -30);
+			ballOne.velY = Math.max(Math.min(randomFloat, 30), -30);
+		}
+		*/
+
 		ballOne.hold({x: launchX, y: launchY});
 		ballOne.release();
 
